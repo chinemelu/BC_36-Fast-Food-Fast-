1faf8f4dc951e89841f77fd7a2de2c5c8d68471b
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-09-19 20:42:33 +0100

    test(LFA feedback): - write new tests for restructured endpoints

--- server/test/cart/fetchAllOrders.js
+++ server/test/cart/fetchAllOrders.js
@@ -21,41 +21,53 @@ describe('GET: /api/v1/orders', () => {
     });
     it('it should fetch all food orders if database has orders', (done) => {
       chai.request(server)
-        .get('/api/v1/cart/add-to-cart/51')
+        .post('/api/v1/orders')
+        .send({
+          userId: 20,
+          items: [{
+            itemId: 51,
+            name: 'burrito',
+            quantity: 24,
+            price: 700
+          }, {
+            itemId: 52,
+            name: 'Meat Pie',
+            quantity: 2,
+            price: 200
+          }, {
+            itemId: 53,
+            name: 'Truffles',
+            quantity: 1,
+            price: 200
+          }]
+        })
         .end(() => {
           chai.request(server)
-            .get('/api/v1/add-to-cart/52')
-            .end(() => {
-              chai.request(server)
-                .post('/api/v1/add-to-cart/53')
-                .end(() => {
-                  chai.request(server)
-                    .post('/api/v1/orders')
-                    .end(() => {
-                      chai.request(server)
-                        .get('/api/v1/orders')
-                        .end((err, res) => {
-                          res.should.have.status(200);
-                          res.body.should.be.a('array');
-                          res.body[0].should.have.property('id');
-                          res.body[0].should.have.property('status');
-                          res.body[0].should.have.property('cart');
-                          res.body[0].id.should.eql(1);
-                          res.body[0].status.should.eql('pending');
-                          res.body[0].cart[0].item.id.should.eql(51);
-                          res.body[0].cart[0].item.name.should.eql('Spaghetti');
-                          res.body[0].cart[0].item.unitPrice.should.eql(650);
-                          res.body[0].cart[0].quantity.should.eql(2);
-                          res.body[0].cart[1].item.id.should.eql(52);
-                          res.body[0].cart[1].item.name.should.eql('Chicken burrito');
-                          res.body[0].cart[1].item.unitPrice.should.eql(250);
-                          res.body[0].cart[2].item.id.should.eql(53);
-                          res.body[0].cart[2].item.name.should.eql('Coke');
-                          res.body[0].cart[2].item.unitPrice.should.eql(100);
-                          done();
-                        });
-                    });
-                });
+            .get('/api/v1/orders')
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              res.body[0].should.have.property('id');
+              res.body[0].should.have.property('status');
+              res.body[0].should.have.property('total');
+              res.body[0].should.have.property('userId');
+              res.body[0].should.have.property('items');
+              res.body[0].id.should.eql(1);
+              res.body[0].total.should.eql(17400);
+              res.body[0].status.should.eql('pending');
+              res.body[0].items[0].itemId.should.eql(51);
+              res.body[0].items[0].name.should.eql('burrito');
+              res.body[0].items[0].price.should.eql(700);
+              res.body[0].items[0].quantity.should.eql(24);
+              res.body[0].items[1].itemId.should.eql(52);
+              res.body[0].items[1].name.should.eql('Meat Pie');
+              res.body[0].items[1].price.should.eql(200);
+              res.body[0].items[1].quantity.should.eql(2);
+              res.body[0].items[2].itemId.should.eql(53);
+              res.body[0].items[2].name.should.eql('Truffles');
+              res.body[0].items[2].price.should.eql(200);
+              res.body[0].items[2].quantity.should.eql(1);
+              done();
             });
         });
     });
--- server/test/cart/fetchOrder.js
+++ server/test/cart/fetchOrder.js
@@ -40,26 +40,37 @@ describe('GET: /api/v1/orders/orderId API route', () => {
   describe('Successful request', () => {
     it('it should fetch a food order if there are no errors', (done) => {
       chai.request(server)
-        .get('/api/v1/orders/1')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.should.have.property('id');
-          res.body.should.have.property('status');
-          res.body.should.have.property('cart');
-          res.body.id.should.eql(1);
-          res.body.status.should.eql('pending');
-          res.body.cart[0].item.id.should.eql(51);
-          res.body.cart[0].item.name.should.eql('Spaghetti');
-          res.body.cart[0].item.unitPrice.should.eql(650);
-          res.body.cart[0].quantity.should.eql(2);
-          res.body.cart[1].item.id.should.eql(52);
-          res.body.cart[1].item.name.should.eql('Chicken burrito');
-          res.body.cart[1].item.unitPrice.should.eql(250);
-          res.body.cart[2].item.id.should.eql(53);
-          res.body.cart[2].item.name.should.eql('Coke');
-          res.body.cart[2].item.unitPrice.should.eql(100);
-          done();
+        .post('/api/v1/orders')
+        .send({
+          userId: 230,
+          items: [{
+            itemId: 54,
+            name: 'Ewedu ati Amala',
+            quantity: 2,
+            price: 1000
+          }]
+        })
+        .end(() => {
+          chai.request(server)
+            .get('/api/v1/orders/2')
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('id');
+              res.body.should.have.property('status');
+              res.body.should.have.property('total');
+              res.body.should.have.property('userId');
+              res.body.should.have.property('items');
+              res.body.id.should.eql(2);
+              res.body.status.should.eql('pending');
+              res.body.total.should.eql(2000);
+              res.body.userId.should.eql(230);
+              res.body.items[0].itemId.should.eql(54);
+              res.body.items[0].name.should.eql('Ewedu ati Amala');
+              res.body.items[0].price.should.eql(1000);
+              res.body.items[0].quantity.should.eql(2);
+              done();
+            });
         });
     });
   });
