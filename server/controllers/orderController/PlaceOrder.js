6330b3c1b920bc60aacb0233c6df22ed9ce7021a
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-09-26 20:41:17 +0100

    feat (place order): - add validator for placing order - add controller for place order feature - delete data structure files and test

---
+++ server/controllers/orderController/PlaceOrder.js
@@ -0,0 +1,62 @@
+import db from '../../models/db';
+
+/**
+ * @class OrderController
+ */
+class OrderController {
+/**
+   * @description place food order
+   *
+   * @param {object} req http request
+   * @param {object} res http response
+   *
+   * @returns {JSON} returns a JSON object
+   */
+  static placeOrder(req, res) {
+    const { userId } = req.decoded;
+
+    const selectText = 'SELECT * FROM carts WHERE user_id = $1';
+    const selectParams = [userId];
+
+    db(selectText, selectParams)
+      .then((cart) => {
+        if (!cart.rows.length) {
+          return res.status(400).json({
+            message: 'Please add at least one item to order'
+          });
+        }
+        const insertText = 'INSERT INTO orders(user_id, cart_id) VALUES ($1, $2) RETURNING \n'
+          + 'id, user_id, order_status';
+
+        const insertParam = [userId, cart.rows[0].id];
+
+        db(insertText, insertParam)
+          .then((order) => {
+            const deleteText = 'delete from carts WHERE id = $1 RETURNING \n'
+            + 'id, total_quantity, total_price, paid_for';
+            const deleteParams = [cart.rows[0].id];
+            db(deleteText, deleteParams, (err) => {
+              if (err) {
+                return res.status(500).json({ error: err.stack });
+              }
+              const updateUserText = 'UPDATE users SET address = $1, phone_number = $2 WHERE id = $3';
+              const updateUserParams = [req.body.address, req.body.mobileNumber, userId];
+              db(updateUserText, updateUserParams, (err) => {
+                if (err) {
+                  return res.status(500).json({ error: err.stack });
+                }
+                res.status(201).json({
+                  message: 'You have ordered successfully',
+                  order: order.rows[0]
+                });
+              });
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({ error: err.stack });
+          });
+      });
+  }
+}
+
+export default OrderController;
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,23 +1,14 @@
 import express from 'express';
-import OrderController from '../controllers/orderController/OrderController';
-import idValidator from '../middleware/idValidator';
+import PlaceOrderController from '../controllers/orderController/PlaceOrder';
+import placeOrderValidator from '../middleware/placeOrderValidator';
+import authenticateToken from '../middleware/authenticateToken';
+import doesUserExist from '../middleware/doesUserExist';
 
 const router = express.Router();
 
-router.get(
-  '/', OrderController.fetchAllOrders
-);
-
-router.get(
-  '/:id', idValidator, OrderController.fetchOrder
-);
-
-router.put(
-  '/:id', idValidator, OrderController.updateOrderStatus
-);
-
 router.post(
-  '/', OrderController.placeOrder
+  '/', placeOrderValidator, authenticateToken, doesUserExist, PlaceOrderController.placeOrder
 );
+
 
 export default router;
