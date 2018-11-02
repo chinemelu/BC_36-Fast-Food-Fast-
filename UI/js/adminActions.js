a9dd16251de3fbeb9c9c86934ae9f5330055debd
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-11-02 05:29:36 +0100

    feat(edit food item): - fix bug in edit item controller - integrate PUT /menu/<itemId> route with frontend

--- UI/js/adminActions.js
+++ UI/js/adminActions.js
@@ -2,22 +2,14 @@ const manageFoodItemBtn = document.querySelector('.manage-food-items'),
   successMessage = document.querySelector('.cd-success-message'),
   errorBannerMessage = document.querySelector('.cd-error-banner-message'),
   foodItemName = document.querySelector('#item-name'),
-  nameErrorMessage = document.querySelector('#add-menu-name-error'),
+  nameErrorMessage = document.querySelector('.add-edit-menu-name-error'),
   foodItemPrice = document.querySelector('#item-price'),
-  priceErrorMessage = document.querySelector('#add-menu-price-error'),
+  priceErrorMessage = document.querySelector('.add-edit-menu-price-error'),
   fileUpload = document.querySelector('#file-upload'),
-  imageErrorMessage = document.querySelector('#add-menu-img-error'),
+  imageErrorMessage = document.querySelector('.add-edit-menu-img-error'),
   adminSpinner = document.querySelector('.spinner'),
-  addToMenuBtn = document.querySelector('.add-food-item-btn');
-
-const editFileUpload = document.createElement('input');
-editFileUpload.type = 'file';
-editFileUpload.className = 'hide';
-document.body.appendChild(editFileUpload);
-
-console.log(editFileUpload);
-const editItemName = createElement('editItemName', 'a', 'edit-item-name');
-const editItemPrice = createElement('editItemPrice', 'a', 'edit-item-name');
+  addToMenuBtn = document.querySelector('.add-food-item-btn'),
+  editFoodItemBtn = document.querySelector('.edit-food-item-btn');
 
 // Cloudinary reference Image Upload in 15 Minutes with Cloudinary and Javascript
 //  Tutorial  by DevCoffee
@@ -46,10 +38,10 @@ const getAllFoodItemsHeader = {
 };
 
 const uploadToCloudinary = (callback) => {
-  if (fileUpload.files.length === 0 && editFileUpload.files.length === 0) {
+  if (fileUpload.files.length === 0) {
     return callback({});
   }
-  const imgFile = fileUpload.files[0] || editFileUpload.files[0];
+  const imgFile = fileUpload.files[0];
   const formData = new FormData();
   formData.append('file', imgFile);
   formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
@@ -63,30 +55,38 @@ const uploadToCloudinary = (callback) => {
 
   return fetch(uploadImageUrl, uploadImageParameters)
     .then(res => res.json())
-    .then(imgUploadResponse => callback(imgUploadResponse));
+    .then(imgUploadResponse => {
+      console.log("got to fetch ==========================")
+      return callback(imgUploadResponse)
+    });
 };
 
 const editItem = (editItemUrl) => {
+  console.log("got here ==========================")
   uploadToCloudinary((cloudinaryResponse) => {
-    const itemDetails = {
-      name: editItemName.value,
-      price: editItemPrice.value,
+    console.log("fooditem name", foodItemName.value)
+    console.log("fooditem price", foodItemPrice.value)
+    console.log("fooditem image", cloudinaryResponse.secure_url)
+
+    const menuDetails = {
+      name: foodItemName.value,
+      price: foodItemPrice.value,
       imgUrl: cloudinaryResponse.secure_url
     };
-
     const editItemParameters = {
       method: 'PUT',
       mode: 'cors',
-      body: JSON.stringify(itemDetails),
+      body: JSON.stringify(menuDetails),
       headers: myAdminHeaders
     };
-
     adminSpinner.classList.remove('hide');
     fetch(editItemUrl, editItemParameters)
       .then((res) => {
         adminSpinner.classList.add('hide');
         removeClassFromClassList(addItemModal, 'is-visible');
-        if (res.status === 201) {
+        addToMenuBtn.disabled = false;
+
+        if (res.status === 200) {
           manageFoodItemsSectionSelected();
           statusMessage(errorBannerMessage, successMessage, 'You have successfully edited the food item');
         }
@@ -107,42 +107,6 @@ const editItem = (editItemUrl) => {
       });
   });
 };
-
-const openEditItemModal = (getItemUrl) => {
-  adminSpinner.classList.remove('hide');
-  let editFoodItemView = '';
-  fetch(getItemUrl, getAllFoodItemsHeader)
-    .then(res => res.json())
-    .then((item) => {
-      adminSpinner.classList.add('hide');
-      addClassToClassList(editFoodItemModal, 'is-visible');
-      editFoodItemView += `
-        <form>
-          <div class="input-container">
-            <div class="input-label-container"><label class="input-label" for="item-name">Name</label></div>
-            <input type="text" class="input-field" class="edit-item-name" value='${item.item.name}'>
-            <p class="cd-error-message" id="add-menu-name-error"></p>
-          </div>
-          <div class="input-container">
-            <div class="input-label-container"><label class="input-label" for="item-price">Price (Naira)</label></div>
-            <input type="number" class="input-field" class="edit-item-price" value='${item.item.price}'>
-            <p class="cd-error-message" id="add-menu-price-error"></p>
-          </div>
-          <div class="input-container">
-            <div class="input-label-container"><label class="input-label">Image</label></div>
-            <input type="file" name="pic" accept="image/*" id="file-upload">
-            <p class="cd-error-message" id="add-menu-img-error"></p>
-          </div>
-          <div class="input-container">
-            <input type="submit" class="add-food-item-btn" value="Update" onclick="editItem('https://fast-food-fast-chinemelu.herokuapp.com/api/v1/menu/${item.item.id}')">
-          </div>
-       </form>
-      `;
-      document.querySelector('.edit-food-item-modal-container').innerHTML = editFoodItemView;
-    });
-};
-
-console.log(document.querySelector('#edit-item-price'));
 
 const deleteItem = (deleteFromMenuUrl) => {
   adminSpinner.classList.remove('hide');
@@ -187,23 +151,15 @@ const openConfirmationModal = (url, name) => {
   </div>`;
 
   deleteConfirmationModalView += `<div class="delete-food-item-modal-footer">
-  <button class="delete-item-btn">Delete</button>
+  <button class="delete-item-btn" onclick ="deleteItem('${url}')">Delete</button>
   <button class="close-modal-btn">Close</button>	
 </div>`;
 
   document.querySelector('.delete-food-item-modal-content').innerHTML = deleteConfirmationModalView;
   addClassToClassList(deleteConfirmationModal, 'is-visible');
 
-  createElement('deleteButton', 'a', 'delete-item-btn');
-  createElement('closeModalBtn', 'a', 'close-modal-btn');
-  createElement('cancelModalIcon', 'a', 'cancel-delete-food-item-modal');
-
-  const closeModalButton = document.createElement('button');
 
   document.addEventListener('click', (e) => {
-    if (e.target.className === 'delete-item-btn') {
-      deleteItem(url);
-    }
     if (e.target.className === 'close-modal-btn' || e.target.className === 'cancel-delete-food-item-modal') {
       removeClassFromClassList(deleteConfirmationModal, 'is-visible');
     }
@@ -226,34 +182,29 @@ const getAllFoodItems = () => {
       let total = 0;
       foodItems.data.map((foodItem) => {
         foodItemsView += `<tr>
-        <td>${total += 1}</td>
-        <td>${foodItem.name}</td>
-       <td>${foodItem.price}</td>
-      <td><div class="image">
-      <img src=${foodItem.img_url}>
-      </div>
-       </td>
-         <td class="admin-action">
-         <div class="edit-entry">
-          <a href="#" onclick="openEditItemModal('https://fast-food-fast-chinemelu.herokuapp.com/api/v1/menu/${foodItem.id}')"><i class="fa fa-edit edit-item"></i></a>
-          </div> 
-            <div class="delete-entry">
-          <a href="#" onclick="openConfirmationModal('https://fast-food-fast-chinemelu.herokuapp.com/api/v1/menu/${foodItem.id}', '${foodItem.name}')"><i class="fa fa-times delete-item"></i></a>
-          </div>
+          <td>${total += 1}</td>
+          <td>${foodItem.name}</td>
+         <td>${foodItem.price}</td>
+        <td><div class="image">
+        <img src=${foodItem.img_url}>
+        </div>
          </td>
-       </tr>
-       `;
+           <td class="admin-action">
+           <div class="edit-entry">
+            <a href="#" onclick="addEditFoodItem(true, 'https://fast-food-fast-chinemelu.herokuapp.com/api/v1/menu/${foodItem.id}', '${foodItem.name}', '${foodItem.price}')"><i class="fa fa-edit edit-item"></i></a>
+            </div> 
+              <div class="delete-entry">
+            <a href="#" onclick="openConfirmationModal('https://fast-food-fast-chinemelu.herokuapp.com/api/v1/menu/${foodItem.id}', '${foodItem.name}')"><i class="fa fa-times delete-item"></i></a>
+            </div>
+           </td>
+         </tr>
+         `;
         return foodItemsView;
       });
       document.getElementById('manage-food-items-entry').innerHTML = foodItemsView;
     })
     .catch(error => error);
 };
-
-manageFoodItemBtn.addEventListener('click', () => {
-  getAllFoodItems();
-});
-
 
 const addToMenuUrl = 'https://fast-food-fast-chinemelu.herokuapp.com/api/v1/menu';
 
@@ -313,6 +264,53 @@ const imageValidator = () => {
   }
 };
 
+const disableEditBtn = () => {
+  editFoodItemBtn.disabled = true;
+};
+
+const enableEditBtn = () => {
+  editFoodItemBtn.disabled = false;
+};
+
+const onEditNameValidator = () => {
+  if (foodItemName.value.trim() && !(/^[a-zA-Z ]+$/.test(foodItemName.value.trim()))) {
+    addToMenuErrors.name = 'Name should consist of only alphabets';
+    nameErrorMessage.classList.add('is-visible');
+    nameErrorMessage.innerHTML = addToMenuErrors.name;
+    // disableEditBtn();
+  } else {
+    delete (addToMenuErrors.name);
+    nameErrorMessage.classList.remove('is-visible');
+    enableEditBtn();
+  }
+};
+
+const onEditPriceValidator = () => {
+  if (foodItemPrice.value.trim() && !(/^-?\d+\.?\d*$/).test(foodItemPrice.value.trim())) {
+    addToMenuErrors.price = 'Please enter a valid price';
+    priceErrorMessage.classList.add('is-visible');
+    priceErrorMessage.innerHTML = addToMenuErrors.price;
+    // disableEditBtn();
+  } else {
+    delete (addToMenuErrors.price);
+    priceErrorMessage.classList.remove('is-visible');
+    enableEditBtn();
+  }
+};
+
+const onEditImageValidator = () => {
+  if (fileUpload.files.length && !(/\.(jpeg|jpg|gif|png)($|\?)/).test(fileUpload.files[0].name)) {
+    addToMenuErrors.fileUpload = 'Please enter a valid image file';
+    imageErrorMessage.classList.add('is-visible');
+    imageErrorMessage.innerHTML = addToMenuErrors.fileUpload;
+    // disableEditBtn();
+  } else {
+    delete (addToMenuErrors.fileUpload);
+    imageErrorMessage.classList.remove('is-visible');
+    enableEditBtn();
+  }
+};
+
 const onKeyDown = (parameter, errorMessage) => {
   parameter.addEventListener('keydown', () => {
     errorMessage.classList.remove('is-visible');
@@ -321,6 +319,10 @@ const onKeyDown = (parameter, errorMessage) => {
 };
 
 const onAddMenuEvent = (element, event) => {
+  addToMenuBtn.disabled = false;
+  nameErrorMessage.classList.remove('is-visible');
+  priceErrorMessage.classList.remove('is-visible');
+  imageErrorMessage.classList.remove('is-visible');
   element.addEventListener(event, () => {
     if (event === 'blur' && element === foodItemPrice) {
       priceValidator();
@@ -330,6 +332,12 @@ const onAddMenuEvent = (element, event) => {
     }
     if (event === 'blur' && element === fileUpload) {
       imageValidator();
+    }
+    if (event === 'input' && element === foodItemPrice) {
+      priceValidator();
+    }
+    if (event === 'input' && element === foodItemName) {
+      nameValidator();
     }
     if (event === 'change' && element === fileUpload) {
       imageValidator();
@@ -347,17 +355,91 @@ const onAddMenuEvent = (element, event) => {
   });
 };
 
-onAddMenuEvent(fileUpload, 'blur');
-onAddMenuEvent(fileUpload, 'change');
-onAddMenuEvent(foodItemName, 'blur');
-onAddMenuEvent(foodItemPrice, 'blur');
-onAddMenuEvent(addToMenuBtn, 'mouseenter');
-onKeyDown(foodItemName, nameErrorMessage);
-onKeyDown(foodItemPrice, priceErrorMessage);
-onKeyDown(fileUpload, imageErrorMessage);
+const onEditMenuEvent = (element, event) => {
+  enableEditBtn();
+  nameErrorMessage.classList.remove('is-visible');
+  priceErrorMessage.classList.remove('is-visible');
+  imageErrorMessage.classList.remove('is-visible');
+  element.addEventListener(event, () => {
+    if (event === 'blur' && element === foodItemPrice) {
+      onEditPriceValidator();
+    }
+    if (event === 'blur' && element === foodItemName) {
+      onEditNameValidator();
+    }
+    if (event === 'blur' && element === fileUpload) {
+      onEditImageValidator();
+    }
+    if (event === 'input' && element === foodItemPrice) {
+      onEditPriceValidator();
+    }
+    if (event === 'input' && element === foodItemName) {
+      onEditNameValidator();
+    }
+    if (event === 'change' && element === fileUpload) {
+      onEditImageValidator();
+    }
+    if (Object.keys(addToMenuErrors).length === 0) {
+      enableEditBtn();
+    } else {
+      disableEditBtn();
+    }
+  });
+};
+
+editFoodItemBtn.addEventListener('mouseenter', () => {
+  onEditNameValidator();
+  onEditPriceValidator();
+  onEditImageValidator();
+});
+
+const addEditFoodItem = (isEditing, url, name, price) => {
+  editFoodItemBtn.onclick = () => {
+    disableEditBtn();
+    editItem(url);
+  };
+
+  if (isEditing === false) {
+    addClassToClassList(editFoodItemBtn, 'hide');
+    removeClassFromClassList(addToMenuBtn, 'hide');
+    onAddMenuEvent(fileUpload, 'blur');
+    onAddMenuEvent(fileUpload, 'change');
+    onAddMenuEvent(foodItemName, 'blur');
+    onAddMenuEvent(foodItemPrice, 'blur');
+    onAddMenuEvent(foodItemName, 'input');
+    onAddMenuEvent(foodItemPrice, 'input');
+    onAddMenuEvent(addToMenuBtn, 'mouseenter');
+    onKeyDown(foodItemName, nameErrorMessage);
+    onKeyDown(foodItemPrice, priceErrorMessage);
+    onKeyDown(fileUpload, imageErrorMessage);
+
+    foodItemName.value = '';
+    foodItemPrice.value = '';
+    fileUpload.value = '';
+    addToMenuBtn.value = 'Add';
+  } else {
+    removeClassFromClassList(editFoodItemBtn, 'hide');
+    addClassToClassList(addToMenuBtn, 'hide');
+    onEditMenuEvent(fileUpload, 'blur');
+    onEditMenuEvent(fileUpload, 'change');
+    onEditMenuEvent(foodItemName, 'blur');
+    onEditMenuEvent(foodItemPrice, 'blur');
+    onEditMenuEvent(foodItemName, 'input');
+    onEditMenuEvent(foodItemPrice, 'input');
+    onEditMenuEvent(addToMenuBtn, 'mouseenter');
+    onKeyDown(foodItemName, nameErrorMessage);
+    onKeyDown(foodItemPrice, priceErrorMessage);
+    onKeyDown(fileUpload, imageErrorMessage);
+
+    foodItemName.value = name;
+    foodItemPrice.value = price;
+    addClassToClassList(addItemModal, 'is-visible');
+  }
+};
 
 
 addToMenuBtn.addEventListener('click', (e) => {
+  addToMenuBtn.disabled = true;
   e.preventDefault();
   uploadToCloudinary((cloudinaryResponse) => {
     const menuDetails = {
@@ -365,18 +447,17 @@ addToMenuBtn.addEventListener('click', (e) => {
       price: foodItemPrice.value,
       imgUrl: cloudinaryResponse.secure_url
     };
-
     const addToMenuParameters = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify(menuDetails),
       headers: myAdminHeaders
     };
-
     adminSpinner.classList.remove('hide');
     fetch(addToMenuUrl, addToMenuParameters)
       .then((res) => {
         adminSpinner.classList.add('hide');
+        addToMenuBtn.disabled = false;
         removeClassFromClassList(addItemModal, 'is-visible');
         if (res.status === 201) {
           manageFoodItemsSectionSelected();
